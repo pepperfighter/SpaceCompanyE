7bfd9117757f7dfea4596d6e78173918bb659fde
pepperfighter <32882409+pepperfighter@users.noreply.github.com>
2022-06-09 12:47:52 -0400

    Update core.js

--- core.js
+++ core.js
@@ -3,7 +3,7 @@ function calculateEnergyOutput(delta) {
 		return 0;
 	}
 
-	var multiplier = 100 + (Game.stargaze.entries.darkMatter.count * dmBoost);
+	var multiplier = 1 + (Game.stargaze.entries.darkMatter.count * dmBoost + 100);
 
 	// Fixed outputs first
 	var output = (ring*ringOutput) + (swarm*swarmOutput) + (sphere*sphereOutput) + (solarPanel*solarPanelOutput);
